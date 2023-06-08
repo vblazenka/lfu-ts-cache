a8dfd76dea9ac91260c963c166b2c99870611fd4
Vedran Blazenka <blazenka.vedran@gmail.com>
2023-06-08 20:33:16 +0200

    feat: setup vitest for testing

---
+++ src/tests/linked-list.test.ts
@@ -0,0 +1,62 @@
+import { describe, expect, test } from "vitest";
+import { LinkedList, LinkedListNode } from "../linked-list";
+
+describe("LinkedList", () => {
+  test("addFirst", () => {
+    const list = new LinkedList<number>();
+    list.addFirst(1);
+    expect(list.first).toEqual(new LinkedListNode(1));
+    expect(list.count).toEqual(1);
+  });
+
+  test("removeFirst", () => {
+    const list = new LinkedList<number>();
+    list.addFirst(1);
+    list.removeFirst();
+    expect(list.first).toBeNull();
+    expect(list.count).toEqual(0);
+  });
+
+  test("addLast", () => {
+    const list = new LinkedList<number>();
+    list.addLast(1);
+    expect(list.last).toEqual(new LinkedListNode(1));
+    expect(list.count).toEqual(1);
+  });
+
+  test("removeLast", () => {
+    const list = new LinkedList<number>();
+    list.addLast(1);
+    list.removeLast();
+    expect(list.last).toBeNull();
+    expect(list.count).toEqual(0);
+  });
+
+  test("remove", () => {
+    const list = new LinkedList<number>();
+    list.addFirst(1);
+    list.remove(1);
+    expect(list.first).toBeNull();
+    expect(list.count).toEqual(0);
+  });
+
+  test("find", () => {
+    const list = new LinkedList<number>();
+    list.addFirst(1);
+
+    const n1 = list.find(1);
+    const n2 = list.find(2);
+
+    expect(n1).toEqual(n1);
+    expect(n2).toBeNull();
+  });
+
+  test("clear", () => {
+    const list = new LinkedList<number>();
+    list.addFirst(1);
+    list.clear();
+    expect(list.first).toBeNull();
+    expect(list.last).toBeNull();
+    expect(list.count).toEqual(0);
+  });
+});
