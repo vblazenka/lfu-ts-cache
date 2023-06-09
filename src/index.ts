a7f4e24896e6c1e97cb185e3dc0e58a079ddf3f8
Vedran Blazenka <blazenka.vedran@gmail.com>
2023-06-09 22:01:26 +0200

    wip: lfu cache implementation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { LinkedList } from "./linked-list";
 
-
 class FrequencyNode {}
 
 type LFUCacheOptions = {
-  max: number,
-  ttl: number,
-}
+  max: number;
+  ttl: number;
+};
 
 /**
  * LFU algorithm implementation that has a runtime complexity of O(1)
@@ -21,32 +20,13 @@ class LFUCache<K extends {}, V extends {}> {
   private freqList: LinkedList<FrequencyNode> = new LinkedList<FrequencyNode>();
 
   constructor(options: LFUCacheOptions) {
-    this.max = options.max
-    this.ttl = options.ttl
+    this.max = options.max;
+    this.ttl = options.ttl;
   }
 
   get(key: string): any {}
 
-  set(key: K, value: V): void {
-    if (this.max <= 0) return;
-
-    console.log("Inserting ", key, value)
-
-    this.size++;
-  }
+  set(key: K, value: V): void {}
 
   delete(): void {}
 }
-
-/*
-    Usage:
-
-    const options = {}
-
-    const cache = new LFUCache<string, string>(options);
-
-    cache.set("key123", "value123")
- */
-
-const cache = new LFUCache({ max: 100, ttl: 3600 })
-cache.set("my-key", "my-value")
