03ecb8f51f0ae50b0b576f2add5e53bc8a7b4436
Vedran Blazenka <blazenka.vedran@gmail.com>
2023-06-11 10:21:30 +0200

    WIP: working LFU cache

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,123 @@
-import { LinkedList } from "./linked-list";
+import { LinkedList, LinkedListNode } from "./linked-list";
 
-class FrequencyNode {}
+class CacheItem<K, V> {
+  public key: K;
+  public value: V;
+  public frequencyParent: LinkedListNode<FrequencyItem<K, V>>;
+}
 
-type LFUCacheOptions = {
-  max: number;
-  ttl: number;
-};
+class FrequencyItem<K, V> {
+  public freq: number = 0;
+  public entries: Set<CacheItem<K, V>>;
+}
 
-/**
- * LFU algorithm implementation that has a runtime complexity of O(1)
- * for each of the operations: get, set and delete.
- */
-class LFUCache<K extends {}, V extends {}> {
-  private max: number;
-  private ttl: number;
+export class LFUCache<K, V> {
+  private capacity: number;
+  private byKey: Map<string, CacheItem<K, V>> = new Map();
+  private freq: LinkedList<FrequencyItem<K, V>>;
   private size: number = 0;
-  private minFreq: number = 0;
-  private lookupTable = new Map<K, V>();
-  private freqList: LinkedList<FrequencyNode> = new LinkedList<FrequencyNode>();
 
-  constructor(options: LFUCacheOptions) {
-    this.max = options.max;
-    this.ttl = options.ttl;
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.freq = new LinkedList<FrequencyItem<K, V>>();
   }
 
-  get(key: string): any {}
+  get(key: K): V {
+    const item = this.byKey.get(key);
+    if (item) {
+      this.increment(item);
 
-  set(key: K, value: V): void {}
+      return item.value;
+    }
 
-  delete(): void {}
+    return null;
+  }
+
+  set(key: K, value: V): void {
+    if (this.byKey.get(key)) {
+      const item = this.byKey.get(key);
+      item.value = value;
+      this.increment(item);
+    } else {
+      const item = new CacheItem();
+      item.key = key;
+      item.value = value;
+      this.byKey.set(key, item);
+      this.size++;
+
+      if (this.atCapacity()) {
+        this.evict(1);
+      }
+
+      this.increment(item);
+    }
+  }
+
+  private increment(item: CacheItem<K, V>) {
+    const currentFrequency = item.frequencyParent;
+    let nextFrequencyAmount: number;
+    let nextFrequency: LinkedListNode<FrequencyItem<K, V>>;
+
+    if (!currentFrequency) {
+      nextFrequencyAmount = 1;
+      nextFrequency = this.freq?.first;
+    } else {
+      nextFrequencyAmount = currentFrequency.value.freq + 1;
+      nextFrequency = currentFrequency.next;
+    }
+
+    if (!nextFrequency || nextFrequency.value.freq !== nextFrequencyAmount) {
+      const newFrequencyItem = new FrequencyItem();
+      newFrequencyItem.freq = nextFrequencyAmount;
+      newFrequencyItem.entries = new Set<CacheItem<K, V>>();
+
+      if (!currentFrequency) {
+        nextFrequency = this.freq.addFirst(newFrequencyItem);
+      } else {
+        nextFrequency = this.freq.addAfter(currentFrequency, newFrequencyItem);
+      }
+    }
+
+    item.frequencyParent = nextFrequency;
+    nextFrequency.value.entries.add(item);
+    if (currentFrequency) {
+      this.remove(currentFrequency, item);
+    }
+  }
+
+  private evict(count: number) {
+    for (let i = 0; i < count; ) {
+      const item = this.freq.first;
+
+      item.value.entries.forEach((entry) => {
+        if (i < count) {
+          this.byKey.delete(entry.key);
+          this.remove(item, entry);
+          this.size--;
+          i++;
+        }
+      });
+    }
+  }
+
+  private remove(
+    freqItem: LinkedListNode<FrequencyItem<K, V>>,
+    item: CacheItem<K, V>
+  ) {
+    const frequencyItem = freqItem.value;
+
+    frequencyItem.entries.forEach((entry) => {
+      if (entry.key === item.key) {
+        frequencyItem.entries.delete(entry);
+      }
+    });
+
+    if (frequencyItem.entries.size === 0) {
+      this.freq.remove(freqItem);
+    }
+  }
+
+  private atCapacity() {
+    return this.size > this.capacity;
+  }
 }
---
+++ src/tests/lfucache.test.ts
@@ -0,0 +1,39 @@
+import { describe, test, expect } from "vitest";
+import { LFUCache } from "../index";
+
+describe("LFUCache", function () {
+  test("should return null for a key not present in the cache", () => {
+    const lfu = new LFUCache<string, number>(2);
+    expect(lfu.get("missingKey")).toBe(null);
+  });
+
+  test("should return the correct value for a key present in the cache", () => {
+    const lfu = new LFUCache<string, number>(2);
+    lfu.set("key", 100);
+    expect(lfu.get("key")).toBe(100);
+  });
+
+  test("should correctly evict the least frequently used item", () => {
+    const lfu = new LFUCache<string, number>(3);
+    lfu.set("key1", 100);
+    lfu.set("key2", 200);
+    lfu.get("key2");
+    lfu.set("key3", 300);
+    lfu.get("key3");
+    lfu.get("key3");
+
+    lfu.set("key4", 400);
+
+    expect(lfu.get("key1")).toBe(null);
+    expect(lfu.get("key2")).toBe(200);
+    expect(lfu.get("key3")).toBe(300);
+  });
+
+  test("should correctly update the value of an existing key", () => {
+    const lfu = new LFUCache<string, number>(2);
+    lfu.set("key", 100);
+    expect(lfu.get("key")).toBe(100);
+    lfu.set("key", 200);
+    expect(lfu.get("key")).toBe(200);
+  });
+});
